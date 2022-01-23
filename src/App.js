bb16b6da134448b0c825c4669810f8cdb6a43eb5
Al Chyrill Cabrera <acabrera@agsx.net>
2022-01-23 19:37:20 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import ApiService from './api'
 
 function App() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const service = new ApiService()
+
+  const submitBtnClick = async() => {
+    try {
+      console.log(email)
+      let response = await service.login({ email, password })
+
+      console.log(response.data)
+    } catch(err) {
+      console.log(err)
+      alert(err.message)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input type="text" value={email} onChange={(val) => setEmail(val.target.value)}/>
+      </div>
+      <div>
+        <input type="password" value={password} onChange={(val) => setPassword(val.target.value)} />
+      </div>
+      <button onClick={submitBtnClick}>Submit</button>
     </div>
   );
 }
